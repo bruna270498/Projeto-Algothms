5188ff8724496820cbb8067e4e5e5a6eae1a53e2
bruna <brunaalmeida075@gmail.com>
2023-06-01 17:12:56 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código ."""
-    raise NotImplementedError
+    quant = 0
+    if target_time is None:
+        return None
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] <= target_time <= period[1]:
+            quant += 1
+    return quant
