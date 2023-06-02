350d2a981947223d4c209bb161850d640173b323
bruna <brunaalmeida075@gmail.com>
2023-06-02 17:01:07 -0300

    req 4 em andamento

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if first_string == '' or second_string == '':
+        return False
+    first_lower = first_string.lower()
+    second_lower = second_string.lower()
+    first = list(first_lower)
+    second = list(second_lower)
+    half_first = first[len(first) // 2]
+    half_second = second[len(second) // 2]
+    smaller = [i for i in first if i < half_first]
+    bigger = [i for i in first if i > half_first]
+    equal = [i for i in first if i == half_first]
+    smallerr = [i for i in first if i < half_second]
+    biggerr = [i for i in first if i > half_second]
+    equall = [i for i in first if i == half_second]
+    string_first = ''.join(smaller + equal + bigger)
+    string_second = ''.join(smallerr + equall + biggerr)
+    and_anagram = string_first == string_second
+    return (string_first, string_second, and_anagram)
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # Testando se ao passar o numero par ele inverte a string
+    key = 2
+    message = 'Mundo'
+    encrypt = encrypt_message(message, key)
+    assert encrypt == 'odn_uM'
+
+    # Testando se ao passar o numero ímpar ele inverte a string
+    message = 'Paralelepípedo'
+    key = 3
+    encrypt = encrypt_message(message, key)
+    assert encrypt == 'raP_odepípelela'
+
+    # Testando se passar uma str no parametro da key retorna o erro
+    message = 'Ola'
+    key = 'a'
+    try:
+        encrypt_message(message, key)
+    except TypeError as error:
+        assert str(error) == "tipo inválido para key"
+    else:
+        assert False
