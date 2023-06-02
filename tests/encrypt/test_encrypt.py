cf0f7ff2969daa4207d914cd8fa13fb34e2fcc7b
bruna <brunaalmeida075@gmail.com>
2023-06-02 17:46:51 -0300

    requisito 2 passado

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,4 +1,5 @@
 from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
@@ -14,12 +15,26 @@ def test_encrypt_message():
     encrypt = encrypt_message(message, key)
     assert encrypt == 'raP_odepípelela'
 
+    # Testar se passar uma key muito grande ele inverte a menssage
+    message = 'vamo'
+    key = 5
+    encrypt = encrypt_message(message, key)
+    assert encrypt == 'omav'
+
     # Testando se passar uma str no parametro da key retorna o erro
     message = 'Ola'
     key = 'a'
-    try:
+    with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message(message, key)
-    except TypeError as error:
-        assert str(error) == "tipo inválido para key"
-    else:
-        assert False
+    # Testando se na messagem não foi passado uma string
+    message = 3
+    key = 2
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(message, key)
+
+    # Testando se passar string vazia returna vazia
+
+    message = ''
+    key = 5
+    encrypt = encrypt_message(message, key)
+    assert encrypt == ""
